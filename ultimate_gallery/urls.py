12e7f9f4b2e27cfc974b4a06e5da1276d00de335
Robert Maina <robert.maina@student.moringaschool.com>
2021-09-06 13:54:46 +0300

    added admin url in the url patterns

--- ultimate_gallery/urls.py
+++ ultimate_gallery/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns=[
     url('^$',views.landing,name = 'landing'),
     url(r'^home/',views.home,name = 'home'),
     url(r'^details/(?P<image_id>\d+)',views.photo_details,name ='photo_details'),
+    url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^search/', views.search_results, name='search_results')
 ]
 if settings.DEBUG:
